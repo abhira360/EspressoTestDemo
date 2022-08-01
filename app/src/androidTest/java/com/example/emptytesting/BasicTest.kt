07737a2bba30045d10b1377849242606a014c86b
abhishek.r <abhishekrao142@gmail.com>
2022-08-01 17:57:49 +0530

    some test cases

--- app/src/androidTest/java/com/example/emptytesting/BasicTest.kt
+++ app/src/androidTest/java/com/example/emptytesting/BasicTest.kt
@@ -1,12 +1,17 @@
 package com.example.emptytesting
 
 import android.text.InputType
+import android.util.Log
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.typeText
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.CoreMatchers.endsWith
 import org.hamcrest.core.AllOf.allOf
 import org.junit.Rule
@@ -46,84 +51,186 @@ class BasicTest {
     }
 
     @Test
-    fun withContentDescription() {
+    fun withIdTest() {
 
-        val test = onView(withId(R.id.tv_helloWorld))
-        test.check(matches(withContentDescription("ab")))
-
-    }
-
-    @Test
-    fun hasContentDescription() {
-        // has some problems right now
-        onView(allOf(
-            withId(R.id.tv_helloWorld),
-
-        ))
-    }
-
-    @Test
-    fun withTagKey() {
-
-        //not working
-    }
-
-    @Test
-    fun withClassName() {
-        //worked
-        val test = onView(withId(R.id.tv_helloWorld))
-        test.check(matches(ViewMatchers.withClassName(endsWith("MaterialTextView"))))
-    }
-
-    @Test
-    fun withInputType() {
-        //not worked yet
-        val test = onView(withInputType(InputType.TYPE_NULL))
-        test.check(matches(withId(R.id.tv_helloWorld)))
-    }
-
-    @Test
-    fun withResourceName() {
-        //not worked yet
-        val test = onView(withResourceName("colors"))
-            test.check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun withAlphaValue() {
         //working
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+
+        testTextView.check(matches(withId(R.id.tv_helloWorld)))
+        testImageView.check(matches(withId(R.id.iv_main)))
+        testEditText.check(matches(withId(R.id.et_main)))
+        testButton.check(matches(withId(R.id.btn_main)))
+    }
+
+    @Test
+    fun withContentDescriptionTest() {
+
+        //working
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+
+        testTextView.check(matches(withContentDescription("text")))
+        testImageView.check(matches(withContentDescription("image")))
+        testEditText.check(matches(withContentDescription("editText")))
+        testButton.check(matches(withContentDescription("button")))
+
+    }
+
+    @Test
+    fun hasContentDescriptionTest() {
+        // working
+
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+
+        testTextView.check(matches(hasContentDescription()))
+        testImageView.check(matches(hasContentDescription()))
+        testEditText.check(matches(hasContentDescription()))
+        testButton.check(matches(hasContentDescription()))
+
+
+    }
+
+    @Test
+    fun withTagKeyTest() {
+        // don't know what tag key is
+    }
+
+    @Test
+    fun withTagValueTest() {
+
+        //working
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+
+
+        testTextView.check(matches(withTagValue(`is`("1"))))
+        testImageView.check(matches(withTagValue(`is`("2"))))
+        testEditText.check(matches(withTagValue(`is`("3"))))
+        testButton.check(matches(withTagValue(`is`("4"))))
+
+
+
+    }
+
+
+    @Test
+    fun withClassNameTest() {
+        //worked
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+        testTextView.check(matches(ViewMatchers.withClassName(endsWith("MaterialTextView"))))
+        testImageView.check(matches(ViewMatchers.withClassName(endsWith("ImageView"))))
+        testEditText.check(matches(ViewMatchers.withClassName(endsWith("EditText"))))
+        testButton.check(matches(ViewMatchers.withClassName(endsWith("Button"))))
+
+    }
+
+    @Test
+    fun withHintTest() {
+        //working
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+        testTextView.check(matches(withHint("Some")))
+        testEditText.check(matches(withHint("Enter something")))
+        testButton.check(matches(withHint("Button Hint")))
+
+    }
+    @Test
+    fun withInputTypeTest() {
+        //this only used  for editText
+        val testEditText = onView(withId(R.id.et_main))
+        testEditText.check(matches(withInputType(InputType.TYPE_CLASS_DATETIME)))
+
+    }
+
+    @Test
+    fun withResourceNameTest() {
+        //working
+        // resource name is the id of the view
+        val test = onView(withResourceName("cl_main"))
+            test.check(matches(isDisplayed()))
+
+        val testTextView = onView((withResourceName("tv_helloWorld")))
+        val testImageView = onView((withResourceName("iv_main")))
+        val testEditText = onView((withResourceName("et_main")))
+        val testButton = onView(withResourceName("btn_main"))
+        testTextView.check(matches(isDisplayed()))
+        testImageView.check(matches(isDisplayed()))
+        testEditText.check(matches(isDisplayed()))
+        testButton.check(matches(isDisplayed()))
+
+    }
+
+    @Test
+    fun withAlphaValueTest() {
+        //working
+        //it test the percentage of view displayed
         val test = onView(withId(R.id.tv_helloWorld))
         test.check(matches(withAlpha(0.9F)))
+
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        val testImageView = onView(withId(R.id.iv_main))
+        val testEditText = onView(withId(R.id.et_main))
+        val testButton = onView(withId(R.id.btn_main))
+        testTextView.check(matches(withAlpha(0.9F)))
+        testImageView.check(matches(withAlpha(1F)))
+        testEditText.check(matches(withAlpha(1F)))
+        testButton.check(matches(withAlpha(1F)))
+
     }
 
     @Test
-    fun withEffectiveVisibility() {
+    fun withEffectiveVisibilityTest() {
         //WORKING
         val test = onView(withId(R.id.tv_helloWorld))
         test.check(matches(ViewMatchers.withEffectiveVisibility(Visibility.INVISIBLE)))
     }
 
     @Test
-    fun withSpinnerText() {
+    fun withSpinnerTextTest() {
         //to test later
     }
 
     @Test
-    fun withSubString() {
+    fun withSubStringTest() {
         //working
-        val test = onView(withId(R.id.cl_main))
-        test.check(matches(withSubstring("c")))
+        //work for view having text attribute
+//        val test = onView(withId(R.id.btn_main))
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+
+//        test.check(matches(withSubstring("u")))
+        testTextView.check(matches(withSubstring("W")))
+
     }
 
     @Test
-    fun hasLinks() {
+    fun hasLinksTest() {
       //not working yet
+        // it only works for textView
+        val testTextView = onView(withId(R.id.tv_helloWorld))
+        testTextView.check(matches(hasLinks()))
+
 
     }
 
     @Test
-    fun hasTextColor() {
-        val test = onView(ViewMatchers.hasTextColor(R.color.purple_500))
+    fun hasTextColorTest() {
+        //working
+        val test = onView(ViewMatchers.hasTextColor(R.color.black))
         test.check(matches(isDisplayed()))
     }
 
@@ -265,7 +372,7 @@ class BasicTest {
     fun hasChildCountTest() {
         //working
         val test = onView(withId(R.id.cl_main))
-        test.check(matches(hasChildCount(2)))
+        test.check(matches(hasChildCount(4)))
     }
 
     @Test
@@ -293,6 +400,12 @@ class BasicTest {
 
 
 
+    @Test
+    fun tyepTextTest() {
+        val test = onView(withId(R.id.et_main))
+
+        test.perform(typeText("any string"))
+    }
 
 
 
--- app/src/test/java/com/example/emptytesting/ExampleUnitTest.kt
+++ app/src/test/java/com/example/emptytesting/ExampleUnitTest.kt
@@ -10,8 +10,77 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun assertEqualsTest() {
+        val expected = 9
+
+        assertEquals( "actual value is not $expected", expected, 9+0)
     }
+
+    @Test
+    fun assertNotEqualsTest() {
+
+        val ob1 = "object"
+        val ob2 = "objct"
+        assertNotEquals("actual and unexpected are equal", ob1, ob2)
+
+    }
+
+    @Test
+    fun assertTrueTest() {
+
+        val s = "a"
+        val s2 = "a"
+        assertTrue("condition is not true",s==s2)
+    }
+
+
+
+    @Test
+    fun assertFalseTest() {
+
+        val s = "a"
+        val s2 = "a"
+        assertFalse("condition is true",s!=s2)
+    }
+
+    @Test
+    fun assertNotNullTest() {
+
+        val test: String? = "null"
+        assertNotNull("$test object is null",test)
+
+    }
+
+    @Test
+    fun assertNullTest() {
+
+        val test: String? = null
+        assertNull("$test object is not null",test)
+
+    }
+
+    @Test
+    fun assertSameTest() {
+        val ob1 = "3"
+        val ob2 = "3"
+        assertSame("objects are not same",ob1,ob2)
+    }
+
+    @Test
+    fun assertNotSameTest() {
+        val ob1 = 0
+        val ob2 = 10
+        assertNotSame("objects are same",ob1,ob2)
+    }
+
+    @Test
+    fun assertArrayEquals() {
+
+        val expected = arrayOf("a", "b", "c")
+        val result = arrayOf("a", "b", "c")
+        assertArrayEquals(expected,result)
+    }
+
 }
